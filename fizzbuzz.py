26039e9d4a8b7c45344c9d4281b648125a759971
David Watson <davidthewatson@gmail.com>
2012-01-07 13:12:54 -0500

    pep8 déviant version in 2 lines of code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,2 @@
-all = list(xrange(1,101))
-divisible_by_3 = dict((x, 'Fizz') for x in filter(lambda x: x % 3 == 0, all))
-divisible_by_5 = dict((x, 'Buzz') for x in filter(lambda x: x % 5 == 0, all))
-divisible_by_3_and_5 = dict((x, 'FizzBuzz') for x in filter(lambda x: x % 3 == 0 and x % 5 == 0, all))
-remainder = dict((x, x) for x in [x for x in all if not x in divisible_by_3 and not x in divisible_by_5 and not x in divisible_by_3_and_5])
-all_dict = dict(divisible_by_3.items() + divisible_by_5.items() + divisible_by_3_and_5.items() + remainder.items())
-for x in all:
-    print all_dict[x]
+from sys import stdout
+[(lambda x=x: stdout.write('FizzBuzz\n') if x % 3 == 0 and x % 5 == 0 else stdout.write('Fizz\n') if x % 3 == 0 else stdout.write('Buzz\n') if x % 5 == 0 else stdout.write(str(x) + '\n'))() for x in xrange(1,101)]
