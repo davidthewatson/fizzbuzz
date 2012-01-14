d75db79aaf1563f9967b76e46a9b4c4a90c242c2
David Watson <watson@smashcode.com>
2012-01-13 22:37:11 -0500

    Now requires python 3. Not compatible with python < 3.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,1 @@
-from __future__ import print_function
-[(lambda x=x: print('FizzBuzz') if x % 3 == 0 and x % 5 == 0 else print('Fizz') if x % 3 == 0 else print('Buzz') if x % 5 == 0 else print(str(x)))() for x in xrange(1,101)]
+[(lambda x=x: print('FizzBuzz') if x % 3 == 0 and x % 5 == 0 else print('Fizz') if x % 3 == 0 else print('Buzz') if x % 5 == 0 else print(str(x)))() for x in range(1,101)]
